d560444f9da590e6558bc92223eb63281bdf298c
Stephen Emblem <stephenaemblem@gmail.com>
2019-10-16 19:12:27 -0400

    Add basic player movement

--- src/autumn_lisp_game_jam_2019/core.cljs
+++ src/autumn_lisp_game_jam_2019/core.cljs
@@ -1,10 +1,29 @@
 (ns autumn-lisp-game-jam-2019.core
-    (:require ))
+  (:require [autumn-lisp-game-jam-2019.vector :as v]))
 
 (enable-console-print!)
 
+(def player-speed 5)
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:player {:pos [0 0]
+                                   :vel [0 0]}}))
+
+(defn player-vel [vel-direction]
+  (swap! app-state
+         update-in
+         [:player :pos]
+         v/add
+         (v/mult player-speed vel-direction)))
+
+(defn player-movement []
+  (when (js/keyIsDown js/UP_ARROW)
+    (player-vel [0 -1]))
+  (when (js/keyIsDown js/DOWN_ARROW)
+    (player-vel [0 1]))
+  (when (js/keyIsDown js/LEFT_ARROW)
+    (player-vel [-1 0]))
+  (when (js/keyIsDown js/RIGHT_ARROW)
+    (player-vel [1 0])))
 
 (defn setup []
   (js/createCanvas 720 400))
@@ -12,7 +31,18 @@
 (defn draw []
   (js/background 50)
   (js/fill 0)
-  (js/rect 360 200 10 10))
+  (js/rect (/ js/width 2) (/ js/height 2) 10 10)
+  (player-movement)
+  (js/rect (-> @app-state
+               :player
+               :pos
+               v/x)
+           (-> @app-state
+               :player
+               :pos
+               v/y)
+           10
+           10))
 
 (doto js/window
   (aset "setup" setup)
