84e48611ad32d2f77d50b991c4493a53cf1e4466
Stephen Emblem <stephenaemblem@gmail.com>
2020-01-22 20:07:35 -0500

    Remove cond and use if-update to update enemy
    cond breaks once a condition is met so if an enemy has seek and udlr defined it
    won't execute code needed for both behaviors.
    
    With threading and the if-update function both behaviors can be run together
    while still checking if the behavior is defined

--- src/autumn_lisp_game_jam_2019/enemy.cljs
+++ src/autumn_lisp_game_jam_2019/enemy.cljs
@@ -6,12 +6,19 @@
                        [1 0]
                        [-1 0]])
 
+(defn if-update
+   [x pred f]
+  (if (pred x)
+    (f x)
+    x))
+
 (defn create-enemy [pos]
   {:pos pos
    :health 3
+   :seek {:speed 2}
    :udlr {:move-time (js/millis)
           :speed 3
-          :move-interval 1000
+          :move-interval 500
           :current-direction (first (shuffle enemy-directions))
           :directions enemy-directions}}
   #_{:pos pos
@@ -32,13 +39,13 @@
   ;;                          (v/normalize)
   ;;                          (v/mult-vec [4 0])
   ;;                          (v/add %)))
-  (cond (:udlr enemy)
-        (udlr-update-move-time (update enemy :pos #(->> (v/mult (:speed (:udlr enemy))
-                                                                (:current-direction (:udlr enemy)))
-                                                        (v/add %))))
-        (:seek enemy)
-        (update enemy :pos #(->> (v/sub (:pos (:player @app-state)) %)
-                                 (v/normalize)
-                                 (v/mult (:speed (:seek enemy)))
-                                 (v/add %)))
-        :else enemy))
+  (-> enemy
+      (if-update :udlr (fn [e]
+                         (udlr-update-move-time (update e :pos #(->> (v/mult (:speed (:udlr enemy))
+                                                                            (:current-direction (:udlr enemy)))
+                                                                     (v/add %))))))
+      (if-update :seek (fn [e]
+                         (update e :pos #(->> (v/sub (:pos (:player @app-state)) %)
+                                                 (v/normalize)
+                                                 (v/mult (:speed (:seek enemy)))
+                                                 (v/add %)))))))
