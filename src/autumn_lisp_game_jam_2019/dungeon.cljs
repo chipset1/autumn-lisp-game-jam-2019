fb98b6637314ddd2ef8f7bbd7e58cc76a81ba87f
Stephen Emblem <stephenaemblem@gmail.com>
2020-06-19 16:30:03 -0400

    Remove unused code in dungeon.cljs

--- src/autumn_lisp_game_jam_2019/dungeon.cljs
+++ src/autumn_lisp_game_jam_2019/dungeon.cljs
@@ -1,12 +1,1 @@
 (ns autumn-lisp-game-jam-2019.dungeon)
-
-(defn unique [data]
-  (into [] (set data)))
-
-(defn room-has-one-door? [tile-map]
-  (let [top (unique (first tile-map))
-        bottom (unique (last tile-map))
-        left (unique (map first tile-map))
-        right (unique (map last tile-map))]
-    (= '([1] [1] [1] [1 0])
-       (sort [top bottom left right]))))
